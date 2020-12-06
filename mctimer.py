29ee317c92336bf30ae4e021b3a22cff4e89ea04
Sharpieman20 <>
2020-12-05 20:24:51 -0600

    update for pix guy

--- mctimer.py
+++ mctimer.py
@@ -16,10 +16,12 @@ json_file = 'mct_config.json'
 with open(json_file) as json_file:
     data2 = json.load(json_file)
 if data2['borderless'] == 'true':
-     data2['borderless']   
+     data2['borderless']
+    
 else:
     data2['borderless'] = False
-
+    
+NUM_CHARS = 11
 system_type = platform.system()
 if system_type == 'Linux':
     directory = os.path.expanduser(data2['linux_saves'])
@@ -33,9 +35,13 @@ window = tk.Tk()
 bg = BindGlobal(widget=window)
 window.text = tk.StringVar()
 window.text2 = tk.StringVar()
+window.text3 = tk.StringVar()
 rt = time.time()
 old_version = False
+did_change = False
+stage = 0
 count = 0
+ig = 0
 if data2['auto_start'] == 'true':
     click1 = 1
     click2 = 1
@@ -49,7 +55,8 @@ def get_time():
         global last_amount
         global old_version
         global amount2
-        #time.sleep(0.1)
+        global ig
+        global did_change
         latest = max([os.path.join(directory,d) for d in os.listdir(directory)], key=os.path.getmtime)
         
         if system_type == "Linux" or system_type == "Darwin":
@@ -72,25 +79,39 @@ def get_time():
             run_time = str(datetime.timedelta(seconds=amount2, milliseconds=0.5))
     
             if last_amount == amount:
+                ig = 0
                 return run_time[:-3]
             else:
+                did_change = True
                 print(latest + "\nTime: " + run_time)
                 last_amount = amount
+                ig = 0
                 return run_time[:-3]
     except:
+        ig = 1
         return '0:00:00.000'
 
 def window2():
-    greeting = tk.Label(fg=data2['igt_color'], bg=data2['bg_color'], font="Arial 45 bold", textvariable=window.text)
+    font_name = data2['font_name']
+    font_size = data2['font_size']
+    font_modifiers = data2['font_modifiers']
+    font = (font_name, font_size, font_modifiers)
+    greeting = tk.Label(fg=data2['igt_color'], bg=data2['bg_color'], font=font, textvariable=window.text)
     greeting.pack()
-    greeting2 = tk.Label(fg=data2['rta_color'], bg=data2['bg_color'], font="Arial 45 bold", textvariable=window.text2)
+    greeting2 = tk.Label(fg=data2['rta_color'], bg=data2['bg_color'], font=font, textvariable=window.text2)
     greeting2.pack()
+    if data2['use_counter'] == 'true':
+        greeting3 = tk.Label(fg=data2['counter_color'], bg=data2['bg_color'], font=font, textvariable=window.text3)
+        greeting3.pack()
+        bg.gbind(data2['increment'], on_increment_counter)
+        greeting.after(0, update_count)
     bg.gbind(data2['pause'], on_press)
     bg.gbind(data2['reset_start'], on_press2)
     bg.gbind(data2['exit'], clicked3)
-    window.bind("<Button-1>", clicked)
-    window.bind("<Button-3>", clicked2)
+    #window.bind("<Button-1>", clicked)
+    #window.bind("<Button-3>", clicked2)
     greeting.after(0, update_time)
+    greeting.after(0, update_time2)
     window.title("MCtimer")
     window.attributes('-topmost', True)
     window.overrideredirect(data2['borderless'])
@@ -99,7 +120,6 @@ def window2():
 
 def update_time():
     global rt
-    window.text.set(get_time())
     if click1 == 1:
         window.text2.set(real_time())
     elif click1 == 0:
@@ -109,11 +129,29 @@ def update_time():
         window.text2.set("0:00:00.000")
     window.after(data2['rta_update'], update_time)
 
+def update_time2():
+    window.text.set(get_time())
+    window.after(1500, update_time2)
+
+def update_count():
+    count_str = str(count)
+    text_str = ""
+    for i in range(0, int(NUM_CHARS/2)):
+        text_str += " "
+    text_str += count_str
+    for i in range(0, int(NUM_CHARS/2)):
+        text_str += " "
+    window.text3.set(text_str)
+    window.after(data2['rta_update'], update_count)
+
 def on_press(event):
     left_click()
 
 def on_press2(event):
     right_click()
+
+def on_increment_counter(event):
+    increment_counter()
 
 def clicked3(event):
     sys.exit(1)
@@ -134,6 +172,10 @@ def left_click():
 def right_click():
     global click1
     global click2
+    global count
+    global did_change
+    count = 0
+    did_change = True
     if click2 == 1:
         click1 = 0
         click2 = 0
@@ -141,27 +183,39 @@ def right_click():
         click2 = 1
         click1 = 1
 
+def increment_counter():
+    global count
+    count += 1
+
 def real_time():
     global rt
     global click1
     global click2
     global amount2
     global old_version
-    global count
+    global stage
+    global ig
+    global did_change
+    if data2['auto_adjust'] == 'true':
+        if did_change:
+            rt = float(time.time()) - float(amount2)
+            did_change = False
     if data2['auto_start'] == 'true':
-        if get_time() == '0:00:00.000':
+        if ig == 1:
             rt = time.time()
             click1 = 1
             click2 = 1
-            count = 0
+            stage = 0
             return '0:00:00.000'
         elif click1 == 1:
-            if old_version == True and count == 0:
+            if old_version == True and stage == 0:
+                ig = 0
                 rt = float(time.time()) - float(amount2)
                 rtc = str(datetime.timedelta(seconds=rt))
-                count = 1
+                stage = 1
                 return rtc[:-3]
             else:
+                ig = 0
                 rt2 = time.time()
                 real_time = rt2 - rt
                 rtc = str(datetime.timedelta(seconds=real_time))
